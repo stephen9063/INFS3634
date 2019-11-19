51fa99c21717cd185c3f39e62687a69c31068edf
z5098341 <z5098341@ad.unsw.edu.au>
2019-11-19 18:54:17 +1100

    Update MainActivity.java

--- app/src/main/java/com/example/oxforddictionary/MainActivity.java
+++ app/src/main/java/com/example/oxforddictionary/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.oxforddictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBtn = (Button) findViewById(R.id.toSearch);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opensearch();
+            }
+        });
+    }
+    public void opensearch(){
+        Intent intent = new Intent(this, search.class);
+        startActivity(intent);
     }
 }
