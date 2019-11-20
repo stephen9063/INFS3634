13db21a3a827eca5ea35917e068eb5a9c9b6c20c
z5098341 <z5098341@ad.unsw.edu.au>
2019-11-20 12:55:25 +1100

    Create synonyms.java

---
+++ app/src/main/java/com/example/oxforddictionary/synonyms.java
@@ -0,0 +1,14 @@
+package com.example.oxforddictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class synonyms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synonyms);
+    }
+}
