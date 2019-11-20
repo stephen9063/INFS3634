5aff9813e0b9a4f64a0c38bf33298342b815201d
z5098341 <z5098341@ad.unsw.edu.au>
2019-11-20 12:55:20 +1100

    Update MainActivity.java

--- app/src/main/java/com/example/oxforddictionary/MainActivity.java
+++ app/src/main/java/com/example/oxforddictionary/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button searchBtn;
+    private Button synBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         searchBtn = (Button) findViewById(R.id.toSearch);
+        synBtn = (Button) findViewById(R.id.toSynonyms);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 opensearch();
             }
         });
+
+        synBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opensyn();
+            }
+        }));
     }
     public void opensearch(){
         Intent intent = new Intent(this, search.class);
+        startActivity(intent);
+    }
+
+    public void opensyn(){
+        Intent intent = new Intent(this, synonyms.class);
         startActivity(intent);
     }
 }
