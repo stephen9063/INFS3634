e69a2fd337007fc6d9fefb6195a3f5776cbd27d2
z5098341 <z5098341@ad.unsw.edu.au>
2019-11-21 14:23:28 +1100

    Create DefQuizAdapter.java

---
+++ app/src/main/java/com/example/oxforddictionary/DefQuizAdapter.java
@@ -0,0 +1,34 @@
+/*
+package com.example.oxforddictionary;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DefQuizAdapter extends RecyclerView.Adapter<DefQuizAdapter.DefViewHolder> {
+
+    private  String Def;
+    Def
+
+    public void setData(String Def){
+        this.Def = Def;
+    }
+
+    @NonNull
+    @Override
+    public DefQuizAdapter.DefViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DefQuizAdapter.DefViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}*/
+
